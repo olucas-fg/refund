1379a64d8d4e22b3c375f6f5c2bc73d30a79b699
Lucas Figueiredo <olucasfg@outlook.com>
2025-01-21 15:22:16 -0300

    aceitando somente números no valor

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//seleciona os elementos do formulário 
+const amount = document.getElementById("amount")
+
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g , "")
+
+  amount.value = value
+
+  console.log(amount.value)
+}
